977ecb8b3a3bdd64dad3699135ad5925b3ed1d5c
Peter Bourgon <peter@bourgon.org>
2012-10-06 12:21:28 +0200

    No-buffer approach

--- g2s.go
+++ g2s.go
@@ -3,7 +3,6 @@ package g2s
 import (
 	"log"
 	"net"
-	"strings"
 	"time"
 )
 
@@ -13,149 +12,89 @@ const (
 
 type Statsd struct {
 	connection net.Conn
-	interval   time.Duration
-	outgoing   []string
-
-	messages chan sendable
-	shutdown chan chan bool
 }
 
-func NewStatsd(endpoint string, interval time.Duration) (*Statsd, error) {
+func NewStatsd(endpoint string) (*Statsd, error) {
 	connection, err := net.DialTimeout("udp", endpoint, 3*time.Second)
 	if err != nil {
 		return nil, err
 	}
-	s := &Statsd{
+	return &Statsd{
 		connection: connection,
-		interval:   interval,
-		outgoing:   []string{},
+	}, nil
+}
 
-		messages: make(chan sendable),
-		shutdown: make(chan chan bool),
+func (s *Statsd) publish(msg sendable) {
+	// "Multiple goroutines may invoke methods on a Conn simultaneously."
+	// http://golang.org/pkg/net/#Conn
+	buf := []byte(msg.Message())
+	n, err := s.connection.Write(buf)
+	if err != nil {
+		log.Printf(
+			"%s: publish: %s",
+			s.connection.RemoteAddr().String(),
+			err,
+		)
+	} else if n != len(buf) {
+		log.Printf(
+			"%s: publish: short send: %d < %d",
+			s.connection.RemoteAddr().String(),
+			n,
+			len(buf),
+		)
 	}
-	go s.loop()
-	return s, nil
 }
 
 func (s *Statsd) IncrementCounter(bucket string, n int) {
-	s.messages <- &counterUpdate{
+	s.publish(&counterUpdate{
 		bucket: bucket,
 		n:      n,
-	}
+	})
 }
 
 func (s *Statsd) IncrementSampledCounter(bucket string, n int, srate float32) {
-	s.messages <- &counterUpdate{
+	s.publish(&counterUpdate{
 		bucket: bucket,
 		n:      n,
 		sampling: sampling{
 			enabled: true,
 			rate:    srate,
 		},
-	}
+	})
 }
 
 func (s *Statsd) SendTiming(bucket string, ms int) {
-	s.messages <- &timingUpdate{
+	s.publish(&timingUpdate{
 		bucket: bucket,
 		ms:     ms,
-	}
+	})
 }
 
 func (s *Statsd) SendSampledTiming(bucket string, ms int, srate float32) {
-	s.messages <- &timingUpdate{
+	s.publish(&timingUpdate{
 		bucket: bucket,
 		ms:     ms,
 		sampling: sampling{
 			enabled: true,
 			rate:    srate,
 		},
-	}
+	})
 }
 
 func (s *Statsd) UpdateGauge(bucket, val string) {
-	s.messages <- &gaugeUpdate{
+	s.publish(&gaugeUpdate{
 		bucket: bucket,
 		val:    val,
-	}
+	})
 }
+
 func (s *Statsd) UpdateSampledGauge(bucket, val string, srate float32) {
-	s.messages <- &gaugeUpdate{
+	s.publish(&gaugeUpdate{
 		bucket: bucket,
 		val:    val,
 		sampling: sampling{
 			enabled: true,
 			rate:    srate,
 		},
-	}
-}
-
-func (s *Statsd) Shutdown() {
-	q := make(chan bool)
-	s.shutdown <- q
-	<-q
-}
-
-func (s *Statsd) loop() {
-	ticker := time.NewTicker(s.interval)
-	defer ticker.Stop()
-	for {
-		select {
-		case sendable := <-s.messages:
-			s.outgoing = append(s.outgoing, sendable.Message())
-		case <-ticker.C:
-			s.publish()
-		case q := <-s.shutdown:
-			q <- true
-			return
-		}
-	}
-}
-
-// publish attempts to send all the updates that have been collected since
-// the last publish to the statsd server. It batches updates into packets of
-// MessagesPerPacket messages. Failures are logged but not retried.
-func (s *Statsd) publish() {
-	// combine outgoing messages into packets
-	buffer, packets := []string{}, []string{}
-	fold := func() {
-		packets = append(packets, strings.Join(buffer, "\n")+"\n")
-		buffer = []string{}
-	}
-	for _, msg := range s.outgoing {
-		buffer = append(buffer, msg)
-		if len(buffer) >= MessagesPerPacket {
-			fold()
-		}
-	}
-	if len(buffer) > 0 {
-		fold()
-	}
-
-	// send each packet
-	N := len(packets)
-	for i, packet := range packets {
-		n, err := s.connection.Write([]byte(packet))
-		if err != nil {
-			log.Printf(
-				"%s: publish %d/%d: %s",
-				s.connection.RemoteAddr().String(),
-				i+1,
-				N,
-				err,
-			)
-		} else if n < len(packet) {
-			log.Printf(
-				"%s: publish %d/%d: short send: %d < %d",
-				s.connection.RemoteAddr().String(),
-				i+1,
-				N,
-				n,
-				len(packet),
-			)
-		}
-	}
-
-	// statsd is best-effort stuff, so reset the outgoing mailbox
-	s.outgoing = []string{}
+	})
 }
--- g2s_test.go
+++ g2s_test.go
@@ -1,39 +1,35 @@
 package g2s
 
 import (
-	"strings"
-	"testing"
-	"sync"
 	"fmt"
-	"net"
-	"time"
 	"math/rand"
+	"net"
+	"strings"
+	"sync"
+	"testing"
+	"time"
 )
 
 func TestPublish1(t *testing.T) {
-	d := 25 * time.Millisecond
 	mock := NewMockStatsd(t, 12345)
-	sd, err := NewStatsd("localhost:12345", d)
+	sd, err := NewStatsd("localhost:12345")
 	if err != nil {
 		t.Fatalf("%s", err)
 	}
 
 	sd.IncrementCounter("foo", 1)
-	time.Sleep(2 * d)
+	time.Sleep(25 * time.Millisecond)
+
 	if lines := mock.Lines(); lines != 1 {
 		t.Errorf("expected 1 line, got %d", lines)
 	}
-	if packets := mock.Packets(); packets != 1 {
-		t.Errorf("expected 1 packet, got %d", packets)
-	}
 
 	mock.Shutdown()
 }
 
-func TestPublishManyLines(t *testing.T) {
-	d := 25 * time.Millisecond
+func TestPublishMany(t *testing.T) {
 	mock := NewMockStatsd(t, 12345)
-	sd, err := NewStatsd("localhost:12345", d)
+	sd, err := NewStatsd("localhost:12345")
 	if err != nil {
 		t.Fatalf("%s", err)
 	}
@@ -41,67 +37,33 @@ func TestPublishManyLines(t *testing.T) {
 	sd.IncrementCounter("foo", 1)
 	sd.SendSampledTiming("bar", 201, 0.1)
 	sd.UpdateGauge("baz", "green")
-	time.Sleep(2 * d)
-	if lines := mock.Lines(); lines != 3 {
-		t.Errorf("expected 3 lines, got %d", lines)
-	}
-	if packets := mock.Packets(); packets != 1 {
-		t.Errorf("expected 1 packet, got %d", packets)
-	}
-
-	mock.Shutdown()
-}
-
-func TestPublishManyPackets(t *testing.T) {
-	d := 25 * time.Millisecond
-	mock := NewMockStatsd(t, 12345)
-	sd, err := NewStatsd("localhost:12345", d)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-
-	sd.IncrementCounter("foo", 1)
-	time.Sleep(2 * d)
-	sd.SendSampledTiming("bar", 201, 0.1)
-	sd.UpdateGauge("baz", "green")
-	time.Sleep(2 * d)
+	time.Sleep(25 * time.Millisecond)
 
 	if lines := mock.Lines(); lines != 3 {
 		t.Errorf("expected 3 lines, got %d", lines)
-	}
-	if packets := mock.Packets(); packets != 2 {
-		t.Errorf("expected 2 packets, got %d", packets)
 	}
 
 	mock.Shutdown()
 }
 
 func TestLoad(t *testing.T) {
-	d := 25 * time.Millisecond
 	mock := NewMockStatsd(t, 12345)
-	sd, err := NewStatsd("localhost:12345", d)
+	sd, err := NewStatsd("localhost:12345")
 	if err != nil {
 		t.Fatalf("%s", err)
 	}
 
-	sends := 1234 // careful: too high, and we take >d ms to send
+	sends := 1234 // careful: too high, and we take too long to send
 	for i := 0; i < sends; i++ {
 		bucket := fmt.Sprintf("bucket-%02d", i%23)
 		n := rand.Intn(100)
 		sd.SendTiming(bucket, n)
 	}
-	time.Sleep(2 * d)
+	time.Sleep(50 * time.Millisecond)
 
 	expectedLines := sends
 	if lines := mock.Lines(); lines != expectedLines {
 		t.Errorf("expected %d lines, got %d", expectedLines, lines)
-	}
-	expectedPackets := sends / MessagesPerPacket
-	if sends%MessagesPerPacket != 0 {
-		expectedPackets++
-	}
-	if packets := mock.Packets(); packets != expectedPackets {
-		t.Errorf("expected %d packets, got %d", expectedPackets, packets)
 	}
 
 	mock.Shutdown()
@@ -112,13 +74,12 @@ func TestLoad(t *testing.T) {
 //
 
 type MockStatsd struct {
-	t       *testing.T
-	port    int
-	packets int
-	lines   int
-	mtx     sync.Mutex
-	ln      *net.UDPConn
-	done    chan bool
+	t     *testing.T
+	port  int
+	lines int
+	mtx   sync.Mutex
+	ln    *net.UDPConn
+	done  chan bool
 }
 
 func NewMockStatsd(t *testing.T, port int) *MockStatsd {
@@ -131,11 +92,6 @@ func NewMockStatsd(t *testing.T, port int) *MockStatsd {
 	return m
 }
 
-func (m *MockStatsd) Packets() int {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
-	return m.packets
-}
 func (m *MockStatsd) Lines() int {
 	m.mtx.Lock()
 	defer m.mtx.Unlock()
@@ -168,7 +124,6 @@ func (m *MockStatsd) loop() {
 		s := strings.TrimSpace(string(b[:n]))
 		m.t.Logf("Mock Statsd: read %dB: %s", n, s)
 		m.mtx.Lock()
-		m.packets++
 		m.lines += len(strings.Split(s, "\n"))
 		m.mtx.Unlock()
 	}
