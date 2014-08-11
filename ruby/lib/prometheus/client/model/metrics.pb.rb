9b4a234aecca95d9476c4f6f556213cee8400a1a
Nick Stenning <nick@whiteink.com>
2014-08-11 18:01:04 +0100

    Squashed '_vendor/src/github.com/prometheus/client_model/' content from commit 775af94
    git-subtree-dir: _vendor/src/github.com/prometheus/client_model
    git-subtree-split: 775af9440e07a658e13f699448c745367bc12115

---
+++ ruby/lib/prometheus/client/model/metrics.pb.rb
@@ -0,0 +1,90 @@
+## Generated from metrics.proto for io.prometheus.client
+require "beefcake"
+
+module Prometheus
+  module Client
+
+    module MetricType
+      COUNTER = 0
+      GAUGE = 1
+      SUMMARY = 2
+      UNTYPED = 3
+    end
+
+    class LabelPair
+      include Beefcake::Message
+    end
+
+    class Gauge
+      include Beefcake::Message
+    end
+
+    class Counter
+      include Beefcake::Message
+    end
+
+    class Quantile
+      include Beefcake::Message
+    end
+
+    class Summary
+      include Beefcake::Message
+    end
+
+    class Untyped
+      include Beefcake::Message
+    end
+
+    class Metric
+      include Beefcake::Message
+    end
+
+    class MetricFamily
+      include Beefcake::Message
+    end
+
+    class LabelPair
+      optional :name, :string, 1
+      optional :value, :string, 2
+    end
+
+    class Gauge
+      optional :value, :double, 1
+    end
+
+    class Counter
+      optional :value, :double, 1
+    end
+
+    class Quantile
+      optional :quantile, :double, 1
+      optional :value, :double, 2
+    end
+
+    class Summary
+      optional :sample_count, :uint64, 1
+      optional :sample_sum, :double, 2
+      repeated :quantile, Quantile, 3
+    end
+
+    class Untyped
+      optional :value, :double, 1
+    end
+
+    class Metric
+      repeated :label, LabelPair, 1
+      optional :gauge, Gauge, 2
+      optional :counter, Counter, 3
+      optional :summary, Summary, 4
+      optional :untyped, Untyped, 5
+      optional :timestamp_ms, :int64, 6
+    end
+
+    class MetricFamily
+      optional :name, :string, 1
+      optional :help, :string, 2
+      optional :type, MetricType, 3
+      repeated :metric, Metric, 4
+    end
+  end
+end
